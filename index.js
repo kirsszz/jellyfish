4fbecd74d9ada965de4a497731dd80a81528c17f
Kirsikka <kirsikka.tuisk@postimeesgrupp.ee>
2022-10-21 18:50:02 +0300

    commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const port = process.env.PORT || 3000;
+
+const indexRoutes = require('./routes/indexRouter');
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.set('view engine', 'html');
+
+app.use('/public', express.static('public'));
+
+app.use('/', indexRoutes);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
